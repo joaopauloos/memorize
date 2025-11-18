7fcdeaa876ecb68bdf09eee1c256722ce1e32176
Joao Paulo <jdjpaulo8@gmail.com>
2025-11-18 19:32:09 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Joao Paulo on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
